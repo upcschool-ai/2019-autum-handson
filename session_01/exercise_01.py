16bcfa2e8dda7e68c28a48a6b3797e61efe0859b
Issey Masuda <issey@vilynx.com>
2019-11-13 00:24:03 +0100

    S01: Exercise 1

---
+++ session_01/exercise_01.py
@@ -0,0 +1,4 @@
+class DataDistribution:
+    # TODO: 1. create `__init__` to initialize the class with random W and b
+    # TODO: 2. implement method `generate` to draw samples from this distribution
+    pass
