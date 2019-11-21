49ecfda05fdf0f5f61a7ea95601919989fcd79c6
Issey Masuda <issey@vilynx.com>
2019-11-21 13:03:57 +0100

    S02: Exercises 2-4

---
+++ session_02/exercise.py
@@ -0,0 +1,11 @@
+def main():
+    # TODO: E02: Define Linear Regressor graph --> Definition phase (use graph, placeholder, variable, operations)
+
+    # TODO: E03: Run a forward pass --> Run phase (use session and the DataDistribution class from previous exercise)
+
+    # TODO: E04: Implement optimization step manually!
+    pass
+
+
+if __name__ == '__main__':
+    main()
