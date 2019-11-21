dee0e7cc3bda9eaad4fa5b7af718f36d309fef89
Issey Masuda <issey@vilynx.com>
2019-11-21 12:54:09 +0100

    S01: Improve solution E01

--- session_01/exercise_01.py
+++ session_01/exercise_01.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 
 
@@ -6,11 +7,13 @@ class DataDistribution:
         self.W = W or random.uniform(-5, 5)
         self.b = b or random.uniform(-5, 5)
 
-    def generate(self):
-        while True:
+    def generate(self, num_iters=None):
+        for step in itertools.count(0, 1):
+            if num_iters is not None and num_iters == step:
+                break
             x = random.uniform(-200, 200)
             y = self.W * x + self.b
             yield x, y
 
-    def __call__(self):
-        return self.generate()
+    def __call__(self, num_iters=None):
+        return self.generate(num_iters=num_iters)
