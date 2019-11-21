324777b4c97a04856d8b6a88d0e043b544207093
Issey Masuda <issey@vilynx.com>
2019-11-21 17:35:06 +0100

    S02: Solution E04

--- sessions/session_02/exercise.py
+++ sessions/session_02/exercise.py
@@ -1,28 +1,61 @@
+import argparse
+import os
+
 import tensorflow as tf
 
 from sessions.session_01 import exercise_01
 
 
-def main():
+def main(learning_rate, logdir):
     graph = tf.Graph()
     with graph.as_default():
-        x = tf.placeholder(tf.float32, shape=[], name='x')
-        W = tf.get_variable('W', shape=[], dtype=tf.float32, initializer=tf.initializers.random_normal())
-        b = tf.get_variable('b', shape=[], dtype=tf.float32, initializer=tf.initializers.random_normal())
+        # Inputs and labels
+        x = tf.placeholder(dtype=tf.float32, name='x')
+        y = tf.placeholder(dtype=tf.float32, name='y')
+
+        # ------------------------ FORWARD PASS -----------------------------------
+        # Linear regression forward pass
+        W = tf.get_variable('W', shape=[], dtype=tf.float32)
+        b = tf.get_variable('b', shape=[], dtype=tf.float32)
         z = W * x
-        y = z + b
+        y_ = z + b
+
+        # Compute loss
+        diff = y_ - y
+        loss = tf.pow(diff, 2)
+
+        # ------------------------ BACKWARD PASS -----------------------------------
+        # Loss backprop
+        loss_grad = 2 * diff
+
+        # Linear regression backprop
+        W_grad = x * loss_grad
+        b_grad = 1 * loss_grad
+        x_grad = W * loss_grad
+
+        # ------------------------ OPTIMIZATION -----------------------------------
+        W_update = W.assign(W - learning_rate * W_grad)
+        b_update = b.assign(b - learning_rate * b_grad)
+        train_op = tf.group(W_update, b_update)
 
     sess = tf.Session(graph=graph)
     with sess:
         sess.run(tf.global_variables_initializer())
         data = exercise_01.DataDistribution()
-        for input_data, label in data.generate(num_iters=10):
-            prediction = sess.run(y, feed_dict={x: input_data})
-            print('Input: {} --> Prediction {}. Label {}'.format(input_data, prediction, label))
+        for input_data, label in data.generate(num_iters=5000):
+            prediction, loss_val, _ = sess.run([y_, loss, train_op], feed_dict={x: input_data, y: label})
+            print('[Loss: {}] Input: {} --> Prediction {}. Label {}'.format(loss_val, input_data, prediction, label))
+        W_pred, b_pred = sess.run([W, b])
+        print('W GT: {}. W pred: {}'.format(data.W, W_pred))
+        print('b GT: {}. b pred: {}'.format(data.b, b_pred))
 
-    # TODO: E04: Implement optimization step manually!
-    pass
+    writer = tf.summary.FileWriter(os.path.expanduser(logdir), graph=graph)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-lr', '--learning_rate', type=float, default=10e-6,
+                        help='Learning rate for the optimization step')
+    parser.add_argument('-l', '--logdir', help='Log dir for tfevents')
+    args = parser.parse_args()
+    main(args.learning_rate, args.logdir)
