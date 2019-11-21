81947681da022de1698b48afd13e3be12f5a8e20
Issey Masuda <issey@vilynx.com>
2019-11-21 16:21:23 +0100

    S02: Solution E02

--- session_02/exercise.py
+++ session_02/exercise.py
@@ -1,5 +1,14 @@
+import tensorflow as tf
+
+
 def main():
-    # TODO: E02: Define Linear Regressor graph --> Definition phase (use graph, placeholder, variable, operations)
+    graph = tf.Graph()
+    with graph.as_default():
+        x = tf.placeholder(tf.float32, shape=[], name='x')
+        W = tf.get_variable('W', shape=[], dtype=tf.float32, initializer=tf.initializers.random_normal())
+        b = tf.get_variable('b', shape=[], dtype=tf.float32, initializer=tf.initializers.random_normal())
+        z = W * x
+        y = z + b
 
     # TODO: E03: Run a forward pass --> Run phase (use session and the DataDistribution class from previous exercise)
 
